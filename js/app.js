1889552009cbe49f92a6c65372b363c677cbe741
afvc <anafilipavc@gmail.com>
2017-03-21 18:39:28 +0000

    )

--- js/app.js
+++ js/app.js
@@ -1,39 +1,54 @@
- function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+ function tplawesome(e, t) {
+     res = e;
+     for (var n = 0; n < t.length; n++) {
+         res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) {
+             return t[n][r]
+         })
+     }
+     return res
+ }
 
-$(function() {
-    $("form").on("submit", function(e) {
-       e.preventDefault();//the default is sumbission
-       // prepare the request
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),//search input value
-            maxResults: 10,
-            order: "viewCount",
-            publishedAfter: "1999-01-01T00:00:00Z"
-       }); 
-       // execute the request
-       request.execute(function(response) {
-          console.log(response);
-          var results=response.result;
+ $(function () {
+             $("form").on("submit", function (e) {
+                 e.preventDefault(); //the default is sumbission
+                 // prepare the request
+                 var request = gapi.client.youtube.search.list({
+                     part: "snippet",
+                     type: "video",
+                     q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"), //search input value
+                     maxResults: 10,
+                     order: "viewCount",
+                     publishedAfter: "1999-01-01T00:00:00Z"
+                 });
+                 // execute the request
+                 request.execute(function (response) {
+                     console.log(response);
+                     var results = response.result;
 
-          $.each(results, function(index, item){
-          
-                $.get("tpl/item.html", function(data){
+                     $.each(results, function (index, item) {
 
-                $("#results").append(tplawesome(data, [{"title":item.snippet.title,"videoid":id.videiID }]);  });
+                         console.log("is showing" + item.snippet.title + "  " + videoID);
+
+                         $.get("tpl/item.html", function (data) {
+
+                             $("#results").append(tplawesome(data, [{
+                                 "title": item.snippet.title,
+                                 "videoid": id.videoID
+                             }]));
+
+                         });
+
+                     });
+                 });
+
+                 $(window).on("resize", resetVideoHeight);
              });
-          });
-    });
-    
-     $(window).on("resize", resetVideoHeight);
-});
 
- 
 
-function init() {
-    gapi.client.setApiKey("AIzaSyAX9MNHEELUlStowPlfYfbzpCoIwUBgx2M");
-    gapi.client.load("youtube", "v3", function() {
-        // yt api is ready
-    });
-}
+
+             function init() {
+                 gapi.client.setApiKey("AIzaSyAX9MNHEELUlStowPlfYfbzpCoIwUBgx2M");
+                 gapi.client.load("youtube", "v3", function () {
+                     // yt api is ready
+                 });
+             }
